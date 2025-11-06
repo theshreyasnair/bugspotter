3146b4f2efe20d7d6d5d44241159e3df2bc574a2
Shreyas Nair <shreyasnair@wifi-10-42-255-64.wifi.berkeley.edu>
2025-11-05 19:26:03 -0800

    created rudimentary parsing model

--- backend/app/parsers/traceback_parser.py
+++ backend/app/parsers/traceback_parser.py
@@ -1,2 +1,21 @@
-def parse():
-    return
+import re
+
+def parse_traceback(traceback_str: str) -> dict:
+    lines = traceback_str.strip().splitlines()
+
+    if not lines:
+        return {"Error" : "empty traceback"}
+    else:
+        last = lines[-1].lstrip(". ").strip()
+        if ":" in last:
+            error_type, message = last.split(":", 1)
+            error_type = error_type.strip()
+            message = message.strip()
+        else:
+            error_type = last.strip()
+            message = ""
+
+    return {
+        "error_type" : error_type,
+        "message" : message
+    }
